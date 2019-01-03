ba14a1aa12ce0c30c4d82a93487c37ef61ce9644
Volodymyr Sharayenko <grand87@yandex.ru>
2019-01-02 22:23:42 -0800

    Added another model - square 2x2 Added basic python cnn model to traion 2 types classigfier & test

--- dataset/lego/test.bat
+++ dataset/lego/test.bat
@@ -2,5 +2,6 @@ set POVRAY_PATH="C:\Program Files\POV-Ray\v3.7\bin"
 set APPNAME=pvengine64.exe
 
 set current_DIR=%cd%
+set model_name=%1
 
-%POVRAY_PATH%\%APPNAME% /RENDER %current_DIR%\v1\model.ini Output_File_Name=%current_DIR%\images\ /EXIT
+%POVRAY_PATH%\%APPNAME% /RENDER %current_DIR%\%model_name%\model.ini Output_File_Name=%current_DIR%\images\ /EXIT
