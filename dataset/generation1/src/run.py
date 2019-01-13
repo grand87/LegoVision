33f289ddce29dca2894d3e1e42a447ebed4f13bd
Volodymyr Sharayenko <grand87@yandex.ru>
2019-01-13 15:27:38 -0800

    Added support to generate set of PNG files per model with zoom\campos\color permutations

--- dataset/generation1/src/run.py
+++ dataset/generation1/src/run.py
@@ -1,11 +1,13 @@
+import itertools
 import json
 import os
 import subprocess
 
 output_dir = "_out"
-ldr_filename = "model.ldr"
-pov_filename = "model.pov"
-
+ldr_filename_template = "model.ldr"
+pov_filename_template = "{zoom}-{lat}-{lon}.pov"
+ldview_cmdline_template = "LDView -WindowWidth=640 -WindowHeight=480 {model} -ExportFile={pov_file} -DefaultZoom={zoom} -cg{lat},{lon}"
+povray_cmdline_template = "pvengine64 /RENDER {inifile} Output_File_Name={outfolder} /EXIT"
 
 def update_pov_file(pov_file_name, model_name):
     pov_file_lines = open(pov_file_name).readlines()
@@ -32,7 +34,7 @@ def main():
     models_file_data = open("config/models.json", "r")
     models_config = json.load(models_file_data)
 
-    color_file_data = open("config/colors.json", "r")
+    color_file_data = open("config/colors5.json", "r")
     color_config = json.load(color_file_data)
 
     template = "1 {color} 0 -24 0 1 0 0 0 1 0 0 0 1 {model}\n"
@@ -45,37 +47,50 @@ def main():
 
     b = False
 
-    for model in models_config["models"]:
-        ld_file = model["ldfile"]
+    workdir = os.getcwd()
 
-        for color_name, color_code in color_config["colors"].items():
+    for r in itertools.product(models_config["models"], color_config["colors"], models_config["zoom"], models_config["campos"]):
+        os.chdir(workdir)
 
-            print("generation model for {0} with color {1}".format(ld_file, color_name))
-            if not b:
-                b = True
-            else:
-                continue
-            ldr_file_content = template.format(model=ld_file, color=color_code)
-            model_dir = os.path.join(output_dir, ldr_dirname_template.format(model=model["name"], color=color_name))
-            if not os.path.exists(model_dir):
-                os.mkdir(model_dir)
+        if not b:
+            b = True
+#        else:
+#            continue
 
-            ldr_file_name = os.path.join(model_dir, ldr_filename)
-            with open(ldr_file_name, "w") as ldr_file:
-                ldr_file.write(ldr_file_content)
+        ld_file = r[0]["ldfile"]
+        color_name = r[1]
+        color_code = color_config["colors"][r[1]]
+        ldr_file_content = template.format(model=ld_file, color=color_code)
 
-            # generation POV file for model
-            pov_file_name = os.path.join(model_dir, pov_filename)
-            pov_export_cmdline = "LDView -WindowWidth=640 -WindowHeight=480 {model} -ExportFile={pov_file} -DefaultZoom=0.2".format(model=ldr_file_name, pov_file=pov_file_name)
-            print("cmdline = {0}".format(pov_export_cmdline))
+        model_dir = os.path.join(output_dir, ldr_dirname_template.format(model=r[0]["name"], color=color_name))
+        if not os.path.exists(model_dir):
+            os.mkdir(model_dir)
 
-            result = subprocess.call(pov_export_cmdline)
+        ldr_file_name = ldr_filename_template
+        ldr_file_name = os.path.join(model_dir, ldr_file_name)
 
-            update_pov_file(pov_file_name, ld_file.split(".")[0])
+        with open(ldr_file_name, "w") as ldr_file:
+            ldr_file.write(ldr_file_content)
 
-            # store ini file for animation
-            with open(pov_file_name+".ini", "w") as model_ini_file_content_new:
-                model_ini_file_content_new.write(model_ini_file_content.format(pov_filename+".mod.pov"))
+        # generation POV file for model
+        pov_filename = pov_filename_template.format(zoom=r[2], lat=r[3]["lat"], lon=r[3]["lon"])
+        pov_filename_full = os.path.join(model_dir, pov_filename)
+        pov_export_cmdline = ldview_cmdline_template.format(model=ldr_file_name, pov_file=pov_filename_full, zoom=r[2], lat=r[3]["lat"], lon=r[3]["lon"])
+        print("cmdline = {0}".format(pov_export_cmdline))
+
+        result = subprocess.call(pov_export_cmdline)
+
+        update_pov_file(pov_filename_full, ld_file.split(".")[0])
+
+        os.chdir(model_dir)
+        # store ini file for animation
+        ini_filename = pov_filename+".ini"
+        with open(ini_filename, "w") as model_ini_file_content_new:
+            model_ini_file_content_new.write(model_ini_file_content.format(pov_filename+".mod.pov"))
+
+        # generate rendering frames
+        povray_cmdline = povray_cmdline_template.format(inifile=ini_filename, outfolder=".")
+        result = subprocess.call(povray_cmdline)
 
     return 0
 
